11ed74044b9a22f391df9fe3c3ff5954effb42b9
henrique.comp <henrique.comp@gmail.com>
2020-03-29 19:09:37 -0300

    initial commit

---
+++ CLR/SCM.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.IO;
+
+namespace CLR
+{
+    public class SCM
+    {
+        [Microsoft.SqlServer.Server.SqlProcedure]
+        public static void SaveFile(SqlString objeto, SqlString sourcePath)
+        {
+
+            using (var connection = new SqlConnection("context connection=true"))
+            {
+                connection.Open();
+                var cmd = new SqlCommand("SELECT object_definition(object_id) FROM sys.procedures WHERE  name = @objeto", connection);
+                cmd.Parameters.AddWithValue("@objeto", objeto);
+                var reader = Convert.ToString(cmd.ExecuteScalar());
+                SqlContext.Pipe.Send("Saving file...");
+                File.WriteAllText(Path.Combine(sourcePath.Value, string.Format("{0}.sql", objeto.Value)), reader);
+                SqlContext.Pipe.Send("File saved.");
+            }
+
+        }
+    }
+}
